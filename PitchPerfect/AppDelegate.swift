3a9fb9a1e42588299dc358c7b4b3fd076d0b92bc
manali78 <manalirami78@gmail.com>
2018-06-17 13:43:13 -0400

    First commit

---
+++ PitchPerfect/AppDelegate.swift
@@ -0,0 +1,43 @@
+//
+//  AppDelegate.swift
+//  PitchPerfect
+//
+//  Created by Manali Rami on 2018-06-14.
+//  Copyright © 2018 Manali Rami. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+       
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+       
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+       
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+    }
+
+
+}
+
